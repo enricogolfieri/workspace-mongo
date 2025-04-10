4958cfc4c170fc6dde51d21f743568be520eeebf
Enrico Golfieri <enrico.golfieri@gmail.com>
2025-04-10 23:09:49 +0200

    added variables for mouting volumes

--- workspace-mongo.plugin.zsh
+++ workspace-mongo.plugin.zsh
@@ -12,30 +12,5 @@ function mongo-build-docker()
 
 function mongo-run-docker()
 {
-    # Start with base command
-    local cmd="docker-compose -f $mongows/docker/docker-compose.mongo.yml"
-  
-    # Add volumes as needed
-    if [ -n "${WORKSPACE_VOLUME}" ]; then
-        cmd="${cmd} -e WORKSPACE_VOLUME=${WORKSPACE_VOLUME}"
-    fi
-
-    if [ -n "${GITIGNORE_VOLUME}" ]; then
-        cmd="${cmd} -e GITIGNORE_VOLUME=${GITIGNORE_VOLUME}"
-    fi
-
-    if [ -n "${GITCONFIG_VOLUME}" ]; then
-        cmd="${cmd} -e GITCONFIG_VOLUME=${GITCONFIG_VOLUME}"
-    fi
-
-    if [ -n "${ZSHRC_VOLUME}" ]; then
-        cmd="${cmd} -e ZSHRC_VOLUME=${ZSHRC_VOLUME}"
-    fi
-
-    if [ -n "${ZSHENV_VOLUME}" ]; then
-        cmd="${cmd} -e ZSHENV_VOLUME=${ZSHENV_VOLUME}"
-    fi
-
-    # Execute the command with any additional arguments
-    eval "${cmd} $@"
+    docker-compose -f $mongows/docker/docker-compose.mongo.yml up
 }
