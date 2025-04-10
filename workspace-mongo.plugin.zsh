14ce79137a8050dc1a0f0258f66af3b4ac778c1b
Enrico Golfieri <enrico.golfieri@gmail.com>
2025-04-10 17:46:10 +0200

    moved docker into workspace mongo

--- workspace-mongo.plugin.zsh
+++ workspace-mongo.plugin.zsh
@@ -5,3 +5,37 @@ mongo-enable() {
     source $mongows/.profile.dev.zsh
 }
 
+function mongo-build-docker()
+{
+    docker-compose -f $mongows/docker/docker-compose.mongo.yml up -d --build dev-mongo
+}
+
+function mongo-run-docker()
+{
+    # Start with base command
+    local cmd="docker-compose -f $mongows/docker/docker-compose.mongo.yml"
+  
+    # Add volumes as needed
+    if [ -n "${WORKSPACE_VOLUME}" ]; then
+        cmd="${cmd} -e WORKSPACE_VOLUME=${WORKSPACE_VOLUME}"
+    fi
+
+    if [ -n "${GITIGNORE_VOLUME}" ]; then
+        cmd="${cmd} -e GITIGNORE_VOLUME=${GITIGNORE_VOLUME}"
+    fi
+
+    if [ -n "${GITCONFIG_VOLUME}" ]; then
+        cmd="${cmd} -e GITCONFIG_VOLUME=${GITCONFIG_VOLUME}"
+    fi
+
+    if [ -n "${ZSHRC_VOLUME}" ]; then
+        cmd="${cmd} -e ZSHRC_VOLUME=${ZSHRC_VOLUME}"
+    fi
+
+    if [ -n "${ZSHENV_VOLUME}" ]; then
+        cmd="${cmd} -e ZSHENV_VOLUME=${ZSHENV_VOLUME}"
+    fi
+
+    # Execute the command with any additional arguments
+    eval "${cmd} $@"
+}
