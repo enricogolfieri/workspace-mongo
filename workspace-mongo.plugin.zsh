445ae0098e9cc013a40c45c75dd4324c7a186a10
Enrico Golfieri <enrico.golfieri@gmail.com>
2025-04-11 11:35:01 +0200

    improved docker

--- workspace-mongo.plugin.zsh
+++ workspace-mongo.plugin.zsh
@@ -12,5 +12,16 @@ function mongo-build-docker()
 
 function mongo-run-docker()
 {
-    docker-compose -f $mongows/docker/docker-compose.mongo.yml up
+    docker-compose -f $mongows/docker/docker-compose.mongo.yml up -d
+}
+
+function mongo-stop-docker()
+{
+    docker-compose -f $mongows/docker/docker-compose.mongo.yml down
+}
+
+#attach to mongo container
+function mongo-attach()
+{
+    docker exec -it dev-mongo-1 zsh
 }
